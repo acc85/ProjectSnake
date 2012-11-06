36c5024315b7a62e68f8b7251a349f211df95ae9
acc85 <acc85@sky.com>
2012-11-06 00:17:14 +0000

    modified randrom drop and hopefully added meta files

--- Assets/Scripts/randomDrop.cs
+++ Assets/Scripts/randomDrop.cs
@@ -2,26 +2,25 @@ using UnityEngine;
 using System.Collections;
 
 public class randomDrop : MonoBehaviour {
-		int time;
+	float timer;
 	// Use this for initialization
 	void Start () {
-		time = 0;
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time ++;
-		if(time == 100)
+		timer +=  Time.deltaTime * 1;
+		Debug.Log (timer);
+		if(timer > 10)
 		{
 			GameObject Sphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
-	//		float x = (float)-9.776898;
-	//		float z = (float)7.720881;
 			float y = (float)44.01806;
 			float randomX = Random.Range((float)0.0, (float)8.0);
 			float randomZ = Random.Range((float)0.0, (float)7.0);
 			Sphere.transform.position = new Vector3(randomX, randomZ, y);
-			time = 0;
+			timer = 0;
 		}
 	}
+	
+	
 }
