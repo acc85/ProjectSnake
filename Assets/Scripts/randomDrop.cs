7f9ce4087acd973f2095fb124b255c68d92778bd
acc85 <acc85@sky.com>
2012-11-07 22:21:42 +0000

    randomise time at which cube will appear

--- Assets/Scripts/randomDrop.cs
+++ Assets/Scripts/randomDrop.cs
@@ -4,17 +4,19 @@ using System.Collections.Generic;
 
 public class randomDrop : MonoBehaviour {
 	float timer;
+	float randomOccurance;
 	
 	public static List<GameObject> cubeObjects;
 	// Use this for initialization
 	void Start () {
 		cubeObjects =  new List<GameObject>();
+		randomOccurance = Random.Range (1,10);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timer +=  Time.deltaTime * 1;
-		if(timer > 2)
+		if(timer > randomOccurance)
 		{
 			GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 			Cube.transform.renderer.material.color = Color.blue;
@@ -23,6 +25,7 @@ public class randomDrop : MonoBehaviour {
 			float randomY = Random.Range((float)-10.0, (float)10.0);
 			Cube.transform.position = new Vector3(randomX, randomY, z);
 			timer = 0;
+			randomOccurance = Random.Range (1,10);
 			cubeObjects.Add(Cube);
 			//globalCubeObjects = cubeObjects;
 			
